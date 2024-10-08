4ad863b5b221ca06ebf4d6334279152b3cde6908
nix@rif <arif@nixs-MacBook-Pro.local>
2024-10-08 09:45:10 +0800

    Initial Commit

---
+++ app/dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+  return (
+    <div className="flex justify-center items-center bg-blue-50 h-screen">
+      <h1 className="">Dashboard</h1>
+      {/* <p className="h-1/2">Try Dashboard</p> */}
+    </div>
+  );
+};
+
+export default Dashboard;
